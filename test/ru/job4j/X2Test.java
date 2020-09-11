72e12a887cc95fd62a1b8d3d7083779d55ddc0fb
stmechanic-dev <kovalev.ilya@mail.ru>
2020-09-11 12:47:41 +0300

    1.1. Входные данные в тесте [#358334]

--- test/ru/job4j/X2Test.java
+++ test/ru/job4j/X2Test.java
@@ -28,23 +28,23 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C0X1Then3() {
+    public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
-        int c = 1;
+        int c = 0;
         int x = 1;
-        int expected = 3;
+        int expected = 2;
         int rls = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rls);
     }
 
     @Test
-    public void whenA1B1C1X0Then3() {
+    public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
-        int x = 1;
-        int expected = 3;
+        int x = 0;
+        int expected = 1;
         int rls = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rls);
     }
