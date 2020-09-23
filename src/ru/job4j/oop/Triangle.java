b85b53e4d2ede72d7039be98878dbbf7e5731a56
stmechanic-dev <kovalev.ilya@mail.ru>
2020-09-24 00:56:48 +0300

    2.2. Рефакторинг - Площадь треугольника. [#358453]

---
+++ src/ru/job4j/oop/Triangle.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        return ((ac + ab) > bc) && ((ac + bc) > ab) && ((ab + bc) > ac);
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            /* написать формулу для расчета площади треугольника. */
+            rsl = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+        return rsl;
+    }
+}
