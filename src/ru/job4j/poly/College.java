ce8f1cc2074824bca47c4bada91d436d458bd7e7
stmechanic-dev <kovalev.ilya@mail.ru>
2020-09-27 01:02:19 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#358410]

---
+++ src/ru/job4j/poly/College.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman fm = new Freshman();
+        Student st = fm;
+        Object obj = st;
+    }
+}
