b13129cb4891a54efa8ed44952ae5636cf47c9df
stmechanic-dev <kovalev.ilya@mail.ru>
2020-10-01 00:55:19 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#358425]

---
+++ src/ru/job4j/oop/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println(new Fact().calc(-2));
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n should be greater than 0");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
