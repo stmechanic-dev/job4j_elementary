4b340c22cba49db213f38340d4dfdf2a906707dd
stmechanic-dev <kovalev.ilya@mail.ru>
2020-10-15 23:51:37 +0300

    4. final [#358445]

---
+++ src/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,30 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printCountEngine() {
+        System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
